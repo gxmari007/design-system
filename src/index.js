740d47cb478378f3b69fdad379a17ec45188f2ab
gxmari007 <392041299@qq.com>
2017-01-07 11:38:55 +0800

    添加入口文件，修改版本号为 0.1.0

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const coview = {};
+
+function install(Vue) {
+  Object.keys(coview).forEach(key => {
+    const component = coview[key];
+
+    Vue.component(component.name, component);
+  });
+}
+
+// 无模块开发环境下自动安装
+if (typeof window !== undefined && window.Vue) {
+  install(window.Vue);
+}
+
+module.exports = {
+  version: '0.1.0',
+};
