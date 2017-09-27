0fc82bc328cf57f1eb0952801b21400356f6bf7a
gxmari007 <392041299@qq.com>
2017-09-28 00:03:24 +0800

    ssr

--- src/index.js
+++ src/index.js
@@ -1,68 +1,68 @@
 // Components
-import CoAffix from './components/affix';
-import CoBackTop from './components/back_top';
-import CoBadge from './components/badge';
-import { CoBreadcrumb, CoBreadcrumbItem } from './components/breadcrumb';
-import { CoButtonGroup, CoButton } from './components/button';
-import CoCard from './components/card';
-import { CoCarousel, CoCarouselItem } from './components/carousel';
-import CoCascader from './components/cascader';
-import { CoCheckboxGroup, CoCheckbox } from './components/checkbox';
-import { CoCollapse, CoCollapsePanel } from './components/collapse';
-import CoColorPicker from './components/color_picker';
-import CoDatePicker from './components/date_picker';
-import { CoDropdown, CoDropdownMenu, CoDropdownItem } from './components/dropdown';
-import { CoForm, CoFormItem } from './components/form';
-import CoInput from './components/input';
-import CoInputNumber from './components/input_number';
+// import CoAffix from './components/affix';
+// import CoBackTop from './components/back_top';
+// import CoBadge from './components/badge';
+// import { CoBreadcrumb, CoBreadcrumbItem } from './components/breadcrumb';
+// import { CoButtonGroup, CoButton } from './components/button';
+// import CoCard from './components/card';
+// import { CoCarousel, CoCarouselItem } from './components/carousel';
+// import CoCascader from './components/cascader';
+// import { CoCheckboxGroup, CoCheckbox } from './components/checkbox';
+// import { CoCollapse, CoCollapsePanel } from './components/collapse';
+// import CoColorPicker from './components/color_picker';
+// import CoDatePicker from './components/date_picker';
+// import { CoDropdown, CoDropdownMenu, CoDropdownItem } from './components/dropdown';
+// import { CoForm, CoFormItem } from './components/form';
+// import CoInput from './components/input';
+// import CoInputNumber from './components/input_number';
 import CoIcon from './components/icon';
 import { CoRow, CoCol } from './components/layout';
-import CoLoading from './components/loading';
+// import CoLoading from './components/loading';
 import { CoMenu, CoMenuItem, CoSubmenu, CoMenuGroup } from './components/menu';
-import CoMessage from './components/message';
-import CoModal from './components/modal';
-import CoPagination from './components/pagination';
-import CoPopover from './components/popover';
-import { CoRadioGroup, CoRadio } from './components/radio';
-import CoRate from './components/rate';
-import { CoSelect, CoOption } from './components/select';
-import { CoSteps, CoStep } from './components/steps';
-import CoSwitch from './components/switch';
-import { CoTable, CoTableColumn } from './components/_table';
-import { CoTabs, CoTabPane } from './components/tabs';
-import CoTag from './components/tag';
-import { CoTimeline, CoTimelineItem } from './components/timeline';
-import CoTooltip from './components/tooltip';
-import CoTree from './components/tree';
+// import CoMessage from './components/message';
+// import CoModal from './components/modal';
+// import CoPagination from './components/pagination';
+// import CoPopover from './components/popover';
+// import { CoRadioGroup, CoRadio } from './components/radio';
+// import CoRate from './components/rate';
+// import { CoSelect, CoOption } from './components/select';
+// import { CoSteps, CoStep } from './components/steps';
+// import CoSwitch from './components/switch';
+// import { CoTable, CoTableColumn } from './components/_table';
+// import { CoTabs, CoTabPane } from './components/tabs';
+// import CoTag from './components/tag';
+// import { CoTimeline, CoTimelineItem } from './components/timeline';
+// import CoTooltip from './components/tooltip';
+// import CoTree from './components/tree';
 // Directives
-import clickoutside from './directives/clickoutside';
+// import clickoutside from './directives/clickoutside';
 // import transferDom from './directives/transfer-dom';
 
 const coview = {
-  CoAffix,
-  CoBackTop,
-  CoBadge,
-  CoBreadcrumb,
-  CoBreadcrumbItem,
-  CoButtonGroup,
-  CoButton,
-  CoCard,
-  CoCarousel,
-  CoCarouselItem,
-  CoCascader,
-  CoCheckboxGroup,
-  CoCheckbox,
-  CoCollapse,
-  CoCollapsePanel,
-  CoColorPicker,
-  CoDatePicker,
-  CoDropdown,
-  CoDropdownMenu,
-  CoDropdownItem,
-  CoForm,
-  CoFormItem,
-  CoInput,
-  CoInputNumber,
+  // CoAffix,
+  // CoBackTop,
+  // CoBadge,
+  // CoBreadcrumb,
+  // CoBreadcrumbItem,
+  // CoButtonGroup,
+  // CoButton,
+  // CoCard,
+  // CoCarousel,
+  // CoCarouselItem,
+  // CoCascader,
+  // CoCheckboxGroup,
+  // CoCheckbox,
+  // CoCollapse,
+  // CoCollapsePanel,
+  // CoColorPicker,
+  // CoDatePicker,
+  // CoDropdown,
+  // CoDropdownMenu,
+  // CoDropdownItem,
+  // CoForm,
+  // CoFormItem,
+  // CoInput,
+  // CoInputNumber,
   CoIcon,
   CoRow,
   CoCol,
@@ -70,26 +70,26 @@ const coview = {
   CoMenuItem,
   CoSubmenu,
   CoMenuGroup,
-  CoModal,
-  CoPagination,
-  CoPopover,
-  CoRadioGroup,
-  CoRadio,
-  CoRate,
-  CoSelect,
-  CoOption,
-  CoSteps,
-  CoStep,
-  CoSwitch,
-  CoTable,
-  CoTableColumn,
-  CoTabs,
-  CoTabPane,
-  CoTag,
-  CoTimeline,
-  CoTimelineItem,
-  CoTooltip,
-  CoTree,
+  // CoModal,
+  // CoPagination,
+  // CoPopover,
+  // CoRadioGroup,
+  // CoRadio,
+  // CoRate,
+  // CoSelect,
+  // CoOption,
+  // CoSteps,
+  // CoStep,
+  // CoSwitch,
+  // CoTable,
+  // CoTableColumn,
+  // CoTabs,
+  // CoTabPane,
+  // CoTag,
+  // CoTimeline,
+  // CoTimelineItem,
+  // CoTooltip,
+  // CoTree,
 };
 
 function install(Vue) {
@@ -99,10 +99,10 @@ function install(Vue) {
     Vue.component(component.name, component);
   });
 
-  Vue.prototype.$loading = CoLoading;
-  Vue.prototype.$message = CoMessage;
+  // Vue.prototype.$loading = CoLoading;
+  // Vue.prototype.$message = CoMessage;
 
-  Vue.directive(clickoutside.name, clickoutside);
+  // Vue.directive(clickoutside.name, clickoutside);
   // Vue.directive(transferDom.name, transferDom);
 }
 
@@ -119,60 +119,60 @@ export default {
 
 export {
   // Components
-  CoAffix,
-  CoBackTop,
-  CoBadge,
-  CoBreadcrumb,
-  CoBreadcrumbItem,
-  CoButtonGroup,
-  CoButton,
-  CoCard,
-  CoCarousel,
-  CoCarouselItem,
-  CoCascader,
-  CoCheckboxGroup,
-  CoCheckbox,
-  CoCollapse,
-  CoCollapsePanel,
-  CoColorPicker,
-  CoDatePicker,
-  CoDropdown,
-  CoDropdownMenu,
-  CoDropdownItem,
-  CoForm,
-  CoFormItem,
-  CoInput,
-  CoInputNumber,
+  // CoAffix,
+  // CoBackTop,
+  // CoBadge,
+  // CoBreadcrumb,
+  // CoBreadcrumbItem,
+  // CoButtonGroup,
+  // CoButton,
+  // CoCard,
+  // CoCarousel,
+  // CoCarouselItem,
+  // CoCascader,
+  // CoCheckboxGroup,
+  // CoCheckbox,
+  // CoCollapse,
+  // CoCollapsePanel,
+  // CoColorPicker,
+  // CoDatePicker,
+  // CoDropdown,
+  // CoDropdownMenu,
+  // CoDropdownItem,
+  // CoForm,
+  // CoFormItem,
+  // CoInput,
+  // CoInputNumber,
   CoIcon,
   CoRow,
   CoCol,
-  CoLoading,
+  // CoLoading,
   CoMenu,
   CoMenuItem,
   CoSubmenu,
   CoMenuGroup,
-  CoMessage,
-  CoModal,
-  CoPagination,
-  CoPopover,
-  CoRadioGroup,
-  CoRadio,
-  CoRate,
-  CoSelect,
-  CoOption,
-  CoSteps,
-  CoStep,
-  CoSwitch,
-  CoTable,
-  CoTableColumn,
-  CoTabs,
-  CoTabPane,
-  CoTag,
-  CoTimeline,
-  CoTimelineItem,
-  CoTooltip,
-  CoTree,
+  // CoMessage,
+  // CoModal,
+  // CoPagination,
+  // CoPopover,
+  // CoRadioGroup,
+  // CoRadio,
+  // CoRate,
+  // CoSelect,
+  // CoOption,
+  // CoSteps,
+  // CoStep,
+  // CoSwitch,
+  // CoTable,
+  // CoTableColumn,
+  // CoTabs,
+  // CoTabPane,
+  // CoTag,
+  // CoTimeline,
+  // CoTimelineItem,
+  // CoTooltip,
+  // CoTree,
   // Directives
-  clickoutside,
+  // clickoutside,
   // transferDom,
 };
