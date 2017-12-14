a734e9f06254733a5a22411a78081085a5120d02
gxmari007 <392041299@qq.com>
2017-12-14 23:22:41 +0800

    :wrench: 按照 airbnb 默认的代码风格

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
   },
   env: {
     browser: true,
@@ -14,31 +14,24 @@ module.exports = {
   plugins: ['html'],
   globals: {
     window: false,
-    document: false
+    document: false,
   },
   // check if imports actually resolve
   settings: {
     'import/resolver': {
       webpack: {
-        config: 'build/webpack.config.resolve.js'
-      }
-    }
+        config: 'build/webpack.config.resolve.js',
+      },
+    },
   },
   // add your custom rules here
   rules: {
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       'js': 'never',
-      'vue': 'never'
+      'vue': 'never',
     }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'no-param-reassign': ['error', { props: false }],
-    'no-unused-expressions': ['error', { allowTernary: true }],
-    'no-shadow': 0,
-    'no-mixed-operators': 0,
-    'global-require': 0,
-    'no-underscore-dangle': ['error', { allowAfterThis: true }],
-    'no-plusplus': [2, { allowForLoopAfterthoughts: true }]
-  }
-}
+  },
+};
