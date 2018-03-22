8fea5bc1230853ae32add2e462c50774ce38a15e
gxmari007 <392041299@qq.com>
2018-03-22 21:13:44 +0800

    mapCoverage 属性已经移除了

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -14,7 +14,6 @@ module.exports = {
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup.js'],
-  mapCoverage: true,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
