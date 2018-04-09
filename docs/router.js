7021cc166b1a3adaedc76bd14628bfecc4e39c2b
gxmari007 <392041299@qq.com>
2018-04-09 21:32:21 +0800

    update

--- docs/router.js
+++ docs/router.js
@@ -1,16 +1,20 @@
 import VueRouter from 'vue-router';
+import Home from '~/pages/the_home';
+import Components from '~/pages/the_components';
 
 const routes = [
   {
     name: 'home',
     path: '/',
     redirect: '/components',
-    component: () => import('~/pages/the_home'),
+    // component: () => import('~/pages/the_home'),
+    component: Home,
   },
   {
     name: 'components',
     path: '/components',
-    component: () => import('~/pages/the_components'),
+    // component: () => import('~/pages/the_components'),
+    component: Components,
   },
 ];
 
