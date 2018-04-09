aafc9d52343ceb424c57c74a364ae8b240445000
gxmari007 <392041299@qq.com>
2018-04-09 21:50:46 +0800

    update

--- build/webpack.docs-prod.conf.js
+++ build/webpack.docs-prod.conf.js
@@ -26,13 +26,13 @@ module.exports = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': config.docsBuild.env
     }),
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        warnings: false
-      },
-      sourceMap: config.docsBuild.sourceMap,
-      parallel: true
-    }),
+    // new UglifyJsPlugin({
+    //   uglifyOptions: {
+    //     warnings: false
+    //   },
+    //   sourceMap: config.docsBuild.sourceMap,
+    //   parallel: true
+    // }),
     // 提取 chunk 中的样式部分打包进单独 css 文件
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css'),
@@ -56,26 +56,26 @@ module.exports = merge(baseWebpackConfig, {
       chunksSortMode: 'dependency'
     }),
     // 作用域提升，浏览器中代码执行的更快
-    // new webpack.optimize.ModuleConcatenationPlugin(),
-    // // 确保 vendor 模块没有变化的情况下 module.id 不会变化
-    // new webpack.HashedModuleIdsPlugin(),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor',
-    //   minChunks(module) {
-    //     return module.resource
-    //       && /\.js/.test(module.resource)
-    //       && module.resource.indexOf(path.join(__dirname, '../node_modules')) === 0
-    //   }
-    // }),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'manifest',
-    //   minChunks: Infinity
-    // }),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'app',
-    //   async: 'vendor-async',
-    //   children: true,
-    //   minChunks: 3
-    // })
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    // 确保 vendor 模块没有变化的情况下 module.id 不会变化
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks(module) {
+        return module.resource
+          && /\.js/.test(module.resource)
+          && module.resource.indexOf(path.join(__dirname, '../node_modules')) === 0
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      minChunks: Infinity
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'app',
+      async: 'vendor-async',
+      children: true,
+      minChunks: 3
+    })
   ]
 });
