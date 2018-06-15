622d8678116c668742dbee741998223504b2abaf
gxmari007 <392041299@qq.com>
2018-06-15 17:38:51 +0800

    update

--- docs/.vuepress/components/demo/button/Basic.vue
+++ docs/.vuepress/components/demo/button/Basic.vue
@@ -11,6 +11,7 @@
         <template slot="title">按钮类型</template>
         <template slot="desc">按钮有四种类型：主按钮、次按钮、虚线按钮、危险按钮。主按钮在同一个操作区域最多出现一次。</template>
       </code-box>
+      <slot />
     </co-col>
   </co-row>
 </template>
